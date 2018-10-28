a31deb9a31de7c8b9d313f2eb82bed7ba5efffb7
EC2 Default User <ec2-user@ip-172-31-71-250.ec2.internal>
2018-10-28 12:22:08 +0000

    Edited dropdown func

---
+++ public/assets/js/cfindex.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropFunctionTopic() {
+  document.getElementById("topicDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-box");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function dropFunctionService() {
+  document.getElementById("serviceDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-box");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}

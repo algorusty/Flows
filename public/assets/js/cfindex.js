c8e0fb9b3054352dedfca6f71a61509f4d9b7bae
mgdante <ec2-user@ip-172-31-71-250.ec2.internal>
2018-10-30 06:49:39 +0000

    changed dropdown to correct markup (select, options) started working on script for dropdowns pending: CSS classes update

--- public/assets/js/cfindex.js
+++ public/assets/js/cfindex.js
@@ -1,13 +1,36 @@
 
+$(document).ready(function(){
+  $("#servicebtn").load("./assets/content/servlist.html");
+});
+
+$(document).ready(function(){
+    $('#servicebtn').on('change', function(){
+        var optionText = $("#servicebtn option:selected").text();
+        var topicIndex = "'./assets/content/topiclist.txt #" + optionText +"'";
+        $("#topicbtn").load(topicIndex);
+        alert(topicIndex);
+    });
+});
+
 $(function(){
   $("#servicebtn").click(function(){
-    $("#serviceDropdown").toggle("fast");
+    $("#serviceDropdown").slideToggle(80);
 });
 });
 
 $(function(){
   $("#topicbtn").click(function(){
-    $("#topicDropdown").toggle("fast");
+    $("#topicDropdown").slideToggle(80);
 });
 });
 
+
+$(document).click(function (e) {
+    e.stopPropagation();
+    var container = $(".dropdown");
+
+    //check if the clicked area is dropDown or not
+    if (container.has(e.target).length === 0) {
+        $('.dropdown-box').slideUp(80);
+    }
+})

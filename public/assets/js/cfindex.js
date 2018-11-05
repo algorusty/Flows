c3efbf7f26cee21c301b2716e5736536bec1b1eb
mgdante <>
2018-11-05 09:24:41 -0400

    fixed js location

--- public/assets/js/cfindex.js
+++ public/assets/js/cfindex.js
@@ -41,30 +41,3 @@ $("document").ready(function(){
     $("#side").load(listIndex);
   });
 });
-
-
-
-/*
-$(function(){
-  $("#servicebtn").click(function(){
-    $("#serviceDropdown").slideToggle(80);
-});
-});
-
-$(function(){
-  $("#topicbtn").click(function(){
-    $("#topicDropdown").slideToggle(80);
-});
-});
-*/
-/*
-$(document).click(function (e) {
-    e.stopPropagation();
-    var container = $(".dropdown");
-
-    //check if the clicked area is dropDown or not
-    if (container.has(e.target).length === 0) {
-        $('.dropdown-box').slideUp(80);
-    }
-});
-*/

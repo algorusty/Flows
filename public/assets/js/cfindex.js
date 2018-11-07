67c2b69d8c73427d870a814a56d348f45eba2d01
mgdante <ec2-user@ip-172-31-71-250.ec2.internal>
2018-11-07 21:03:25 +0000

    added ajax effects, cleaned up script

--- public/assets/js/cfindex.js
+++ public/assets/js/cfindex.js
@@ -1,43 +1,41 @@
+/* global $*/
+
 var vHeight = $(window).height();
     vWidth = $(window).width();
 
 $("body").css({"height":vHeight,"width":vWidth});
 
-
-$(document).ready(function(){
-  //load services list
-  $("#servicebtn").load("./assets/content/servlist.html", function(){
-    //load default topic
-    var optionText = $("#servicebtn option:selected").text();
-    var topicIndex = "./assets/content/topics/" + optionText + ".html";
-    $("#topicbtn").load(topicIndex, function(){
-      //load default side list
+var sideLoad = function(){
+  //fade in topicbtn
+  $('#topicbtn').fadeIn(33.33)
+        //load default side list
       var topicText = $("#topicbtn option:selected").text();
       var listIndex = './assets/content/sidelists/' + topicText.replace(/ +/g, "-") + '.html';
-      $("#side").load(listIndex);
-    });
-  });
-});
-
-$(document).ready(function(){
-    $('#servicebtn').on('change', function(){
+      $('#side').fadeOut(33.33, function(){
+        $('#side').load(listIndex, function(){
+          $('#side').fadeIn(50);
+        });
+      });
+};
+var topicLoad = function(){
       //service option change, load topic
         var optionText = $("#servicebtn option:selected").text();
         var topicIndex = "./assets/content/topics/" + optionText + ".html";
-        $("#topicbtn").load(topicIndex, function(){
-          //load default sidelist
-          var topicText = $("#topicbtn option:selected").text();
-          var listIndex = './assets/content/sidelists/' + topicText.replace(/ +/g, "-") + '.html';
-          $("#side").load(listIndex);
-    });
-  });
+        $('#topicbtn').fadeOut(33.33, function(){
+          $("#topicbtn").load(topicIndex, sideLoad);
+        });
+  }
+
+$(document).ready(function(){
+  //load services list
+  $("#servicebtn").load("./assets/content/servlist.html", topicLoad);
 });
 
-$("document").ready(function(){
-  $("#topicbtn").on('change', function(){
-    // load sidelists
-    var topicText = $("#topicbtn option:selected").text();
-    var listIndex = "./assets/content/sidelists/" + topicText.replace(/ +/g, "-") + ".html";
-    $("#side").load(listIndex);
+$(document).ready(function(){
+    $('#servicebtn').on('change', topicLoad);
   });
+
+$("document").ready(function(){
+  $("#topicbtn").on('change', sideLoad);
 });
+

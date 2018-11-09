bbfa889746fc2670684c03254d38caa902774f70
mgdante <ec2-user@ip-172-31-71-250.ec2.internal>
2018-11-09 06:42:40 +0000

    .

--- public/assets/js/cfindex.js
+++ public/assets/js/cfindex.js
@@ -8,19 +8,22 @@ $("body").css({"height":vHeight,"width":vWidth});
 var sideLoad = function(){
   //fade in topicbtn
   $('#topicbtn').fadeIn(33.33)
-        //load default side list
+        // on topic ready, load sidebar
       var topicText = $("#topicbtn option:selected").text();
       var listIndex = './assets/content/sidelists/' + topicText.replace(/ +/g, "-") + '.html';
+      //fade effect for sidebar
       $('#side').fadeOut(33.33, function(){
         $('#side').load(listIndex, function(){
           $('#side').fadeIn(50);
         });
       });
 };
+
 var topicLoad = function(){
       //service option change, load topic
         var optionText = $("#servicebtn option:selected").text();
         var topicIndex = "./assets/content/topics/" + optionText + ".html";
+        //fade effect for topics
         $('#topicbtn').fadeOut(33.33, function(){
           $("#topicbtn").load(topicIndex, sideLoad);
         });

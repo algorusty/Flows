ec4fd988f47026a8aa46747f60c0786ccb0e895e
Wesley <soto.wesley@gmail.com>
2018-11-25 22:35:03 -0400

    Added barebones portfolio page

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const port = 8080;
 const app = express();
-const morgan = require('morgan')
-const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
 
-const limiter = rateLimit({
-  windowsMs: 15 * 60 * 1000,
-  max: 50
-});
-
-app.use(limiter)
 
 app.listen(port, function() {
   console.log(`Server is running on port ${port}`)
 });
 
-app.use(express.static('public'));
+app.use('/flows', express.static('public'))
+app.use('/', express.static('portfolio'))
 
 app.use(morgan('combined'));

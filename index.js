7a654608b32240200900451f60feefa446d5ed7d
Wesley <soto.wesley@gmail.com>
2018-12-01 05:41:48 -0400

    added fullpage.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express();
 const morgan = require('morgan');
 
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Server is running on port ${port}`)
 });
 
-app.use('/flows', express.static('public'))
-app.use('/', express.static('portfolio'))
+app.use('/portfolio', express.static('portfolio'))
+app.use('/', function (req, res, next) {
+  console.log('Flows accessed');
+  next();
+}, express.static('public'));
 
 app.use(morgan('combined'));
